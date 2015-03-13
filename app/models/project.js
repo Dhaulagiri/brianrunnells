9ef020f3432280c310ff24c12652d7c462f129ec
Brian Runnells <brian.runnells@gmail.com>
2015-03-13 10:53:45 -0500

    Tweak

--- app/models/project.js
+++ app/models/project.js
@@ -17,8 +17,8 @@ ProjectModel.reopenClass({
       title: 'InstaClimb',
       url: 'http://instaclimb.com',
       urlActive: true,
-      snippet: 'Tracking',
-      description: 'InstaClimb is a Ruby on Rails application that integrates with APIs from Facebook, Twitter and Instagram to track the changes in follower counts of tracked users.  This information is presented using Ember.js and HighCharts',
+      snippet: 'Social Media Tracking',
+      description: 'InstaClimb is a Ruby on Rails application that integrates with APIs from Facebook, Twitter and Instagram to track the changes in follower counts of tracked users.  This information is presented using Ember.js and HighCharts.',
       tools: [1, 2, 3, 5, 8]
     },
     {
