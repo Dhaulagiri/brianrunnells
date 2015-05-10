247e9629eef963fe86e64c4e9c356030cea6a9ff
Brian Runnells <brian.runnells@gmail.com>
2015-05-10 10:15:58 -0500

    Ember try

---
+++ config/ember-try.js
@@ -0,0 +1,40 @@
+/* jshint node: true */
+
+module.exports = {
+  scenarios: [
+    {
+      name: 'ember-1.11.1',
+      dependencies: {
+        'ember': '1.11.1',
+        'ember-data': '1.0.0-beta.16'
+      }
+    },
+    {
+      name: 'ember-release',
+      dependencies: {
+        'ember': 'components/ember#release'
+      },
+      resolutions: {
+        'ember': 'release'
+      }
+    },
+    {
+      name: 'ember-beta',
+      dependencies: {
+        'ember': 'components/ember#beta'
+      },
+      resolutions: {
+        'ember': 'beta'
+      }
+    },
+    {
+      name: 'ember-canary',
+      dependencies: {
+        'ember': 'components/ember#canary'
+      },
+      resolutions: {
+        'ember': 'canary'
+      }
+    }
+  ]
+};
