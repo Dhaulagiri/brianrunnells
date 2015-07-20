350c32e3b3b6a0f92f7f27a82030d47537e707ba
Brian Runnells <brian.runnells@gmail.com>
2015-07-19 21:11:49 -0700

    Upgrades
    * ember-cli 1.13.1
    * ember 1.13.3
    * ember-data 1.13.5
    * iojs on travis

--- config/ember-try.js
+++ config/ember-try.js
@@ -3,11 +3,8 @@
 module.exports = {
   scenarios: [
     {
-      name: 'ember-1.11.1',
-      dependencies: {
-        'ember': '1.11.1',
-        'ember-data': '1.0.0-beta.16'
-      }
+      name: 'default',
+      dependencies: { }
     },
     {
       name: 'ember-release',
